ccbe4884fa9e7d93614310fbddf69d0418a5fc06
RGDev <95212617+WSEngineerRG@users.noreply.github.com>
2022-02-14 09:44:04 +0100

    update js

--- JS/index.js
+++ JS/index.js
@@ -2,19 +2,11 @@ const loadFile = function (event) {
     let image = document.getElementById("output");
     image.src = URL.createObjectURL(event.target.files[0]);
 };
-window.addEventListener('load resize', () => {
-    let content = document.querySelectorAll('iframe[src*="embed.spotify.com"]');
-    content.forEach()( () => {
-        content.style.width= content.parentNode.style.width;
-        content.setAttribute.src= content.setAttribute.src;
-        content.classList.remove('loaded');
 
-        content.addEventListener('load', () => {
-            content.classList.add('loaded');
-        });
-    });
-});
+document.addEventListener('contextmenu', event => event.preventDefault());
+
 function search() {
-    href:'https://api.spotify.com/v1/search?<?=$_GET['Search']?>')
-    postMessage('https://embed.spotify.com/search/<?=$_GET['Search']?>')
+
 }
+
+
